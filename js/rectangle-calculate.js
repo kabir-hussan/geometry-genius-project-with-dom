a0a49d89b217854a4af41f9e730dba1d3eb3fa2b
kabir-hussan <kabirhussan891@gmail.com>
2024-02-16 01:23:13 +0600

    rectangle of area calculator added

---
+++ js/rectangle-calculate.js
@@ -0,0 +1,16 @@
+// create rectangle area function
+
+function rectangleAreaCalculate()
+{
+    const rectangleLength= document.getElementById('rectangle-length').value;
+    const length= parseFloat(rectangleLength);
+
+    const rectangleWidth= document.getElementById('rectangle-width').value;
+    const width= parseFloat(rectangleWidth);
+
+    const area= length * width;
+
+    const AreaOfRectangle= document.getElementById('rectangle-area-value');
+    AreaOfRectangle.innerText= area;
+    console.log(AreaOfRectangle);
+}
